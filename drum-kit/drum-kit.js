c5ed65b22bc69610e9cea6c2d932af4b35b76095
manimishra <manimishra902@gmail.com>
2017-05-14 02:16:12 +0530

    finish up drum kit

---
+++ drum-kit/drum-kit.js
@@ -0,0 +1,22 @@
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+  key.addEventListener('transitionend', endTransition);
+});
+
+function endTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+function playSound(e) {
+  const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audioEl);
+  if (!audioEl) return;
+
+  audioEl.currentTime = 0;
+  audioEl.play();
+  keyEl.classList.add('playing');
+}
